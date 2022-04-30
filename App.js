cfaf66c67b511fc0234f133a599852d19ea6fb48
davidobi <dbiggestdave@yahoo.com>
2022-04-30 10:07:26 +0000

    add fonts to the project

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
+import Home from './screens/Home';
+import Details from './screens/Details';
+
 
 const Stack = createStackNavigator();
 
@@ -9,11 +11,18 @@ const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background: "Transparent"
+    background: "transparent"
   }
 }
 
 const App = () => {
+  const [loaded] = useFonts ({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  })
   return (
    <NavigationContainer theme={theme}>
      <Stack.Navigator screenOptions={{ headerShown: false}} initialRoutename = 'Home'>
