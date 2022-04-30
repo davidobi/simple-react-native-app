2c2427e8253607828c544ee6cb80787b86a1e28d
davidobi <dbiggestdave@yahoo.com>
2022-04-30 11:06:12 +0000

    add image and style NFT card component

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { View, SafeAreaView, FlatList, Text } from 'react-native';
 import { COLORS, NFTData } from '../constants';
-import { FocusedStatusBar, HomeHeader, Homeheader, NFTCard } from './components';
+import { FocusedStatusBar, HomeHeader, NFTCard } from '../components';
 
 const Home = () => {
   return (
@@ -11,11 +11,24 @@ const Home = () => {
       <View style={{zIndex: 0}}>
         <FlatList
           data={NFTData}
-          renderItem ={({item}) => <Text>{item.name}</Text>}
+          renderItem ={({item}) => <NFTCard data={item} />}
           keyExtractor={(item) => item.id}
           showsVerticalScrollIndicator={false}
           ListHeaderComponent={<HomeHeader />}
           />
+      </View>
+
+      <View style={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        zIndex: -1,
+      }}>
+        <View style={{height: 300, backgroundColor: COLORS.primary }} />
+        <View style = {{flex:1, backgroundColor: COLORS.white}} />
+
       </View>
      </View>
    </SafeAreaView>
