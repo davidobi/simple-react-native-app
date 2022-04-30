0602f6a8e46610cc7dcb72465d8f9cf92b0c3569
davidobi <dbiggestdave@yahoo.com>
2022-04-30 11:45:32 +0000

    complete the SUbinfo with hardcoded values

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { SIZES, FONTS, COLORS, SHADOWS, assets } from '../constants'
 
 export const NFTTitle = () => {
   return (
@@ -17,34 +18,75 @@ export const ETHPrice = () => {
   )
 }
 
-export const ImageCmp = () => {
+export const ImageCmp = ({imgUrl, index}) => {
   return (
-    <View>
-      <Text>ImageCmp</Text>
-    </View>
+    <Image
+        source={imgUrl}
+        resizeMode='contain'
+        style={{
+            width: 48,
+            height:48,
+            marginLeft: index === 0 ? 0 : -SIZES.font
+        }}
+    />
   )
 }
 
 export const People = () => {
   return (
-    <View>
-      <Text>People</Text>
+    <View style={{
+        flexDirection: 'row'
+    }}>
+      {[assets.person02, assets.person03, assets.person04].map((imgUrl, index) => (
+          <ImageCmp imgUrl={imgUrl} index={index} key={`People-${index}`} />
+      ))}
     </View>
   )
 }
 
 export const EndDate = () => {
   return (
-    <View>
-      <Text>NFTTitle</Text>
+    <View
+        style= {{
+            paddingHorizontal: SIZES.font,
+            paddingVertical: SIZES.base,
+            backgroundColor: COLORS.white,
+            justifyContent: 'center',
+            alighItems: 'center',
+            ...SHADOWS.light,
+            elevation: 1,
+            maxWidth: '50%'
+        }}
+    >
+      <Text style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.primary
+          }}>
+          Ending in 
+        </Text>
+        <Text style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.medium,
+          color: COLORS.primary
+          }}>
+          12h 30m 
+        </Text>
     </View>
   )
 }
 
 export const SubInfo = () => {
   return (
-    <View>
-      <Text>SubInfo</Text>
+    <View style={{
+        width: '100%',
+        paddingHorizontal: SIZES.font,
+        marginTop: -SIZES.extraLarge,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }}>
+      <People />
+      <EndDate />
     </View>
   )
 }
