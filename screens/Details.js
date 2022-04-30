e35a1a85b29e7b2eff00a32a0bceb366d602a06d
davidobi <dbiggestdave@yahoo.com>
2022-04-30 20:07:40 +0000

    add data and read more state to details page

--- screens/Details.js
+++ screens/Details.js
@@ -59,6 +59,10 @@ const DetailsScreen = ({route, navigation}) => {
         ListHeaderComponent={() => (
           <React.Fragment>
             <DetailsHeader data={data} navigation={navigation} />
+            <SubInfo />
+            <View style={{ padding: SIZES.font }}>
+              <DetailsDesc data={data} />
+            </View>
           </React.Fragment>
         )}
       />
