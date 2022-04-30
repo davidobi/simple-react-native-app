bf65698feab41c0f2732b402d7956d65e59355c9
davidobi <dbiggestdave@yahoo.com>
2022-04-30 13:46:28 +0000

    start of details screen design

--- screens/Details.js
+++ screens/Details.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native';
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from '../constants';
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from '../components';
 
 const Details = () => {
   return (
